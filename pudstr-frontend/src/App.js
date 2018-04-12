12d461a2de06632f5e69d3691040bf4c22f571e6
Cerunnos <github email address>
2018-04-12 15:46:25 -0400

    v2

--- pudstr-frontend/src/App.js
+++ pudstr-frontend/src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MapContainer from './MapContainer'
 import './App.css';
+
 
 class App extends Component {
   render() {
+    // const apikey="AIzaSyD4jflUGcUZPACxuTKOufXJG71rPE5uQzg"
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MapContainer/>
       </div>
     );
   }
 }
 
 export default App;
+
+//apikey=AIzaSyD4jflUGcUZPACxuTKOufXJG71rPE5uQzg
---
+++ pudstr-frontend/src/MapContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+export class MapContainer extends Component {
+  state={
+    selectedPlace:{name:"New York City"},
+    currentCoordinates:{lat:40.7053,lng:-74.0140}
+  }
+  render() {
+
+    return (
+      <Map google={this.props.google} zoom={14} initialCenter={this.state.currentCoordinates}>
+
+        <Marker onClick={this.onMarkerClick}
+              name={'Current location'} />
+
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyD4jflUGcUZPACxuTKOufXJG71rPE5uQzg")
+})(MapContainer)
